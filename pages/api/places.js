8fe04a4e1983919f7ca5b68caa4a3bdccb6ff9a8
Raphael Ramos <rrs@brickabode.com>
2021-08-09 23:54:26 -0300

    Add Icons to nearby places

--- pages/api/places.js
+++ pages/api/places.js
@@ -3,11 +3,12 @@ const {Client} = require("@googlemaps/google-maps-services-js");
 export default async function getNearbylaces(req, res) {
 
     const client = new Client({})
-
+    console.log(process.env.MAPS_JAVASCRIPT_API_KEY)
     client
     .placesNearby({
         params: {
             location: { lat: req.query.lat, lng: req.query.lng },
+            // location: { lat: -27.603992659910922, lng: -48.529671737536724 },
             radius: 10000,
             type: "police",
             key: process.env.GOOGLE_MAPS_API_KEY
